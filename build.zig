774a50d79be551a79c8e8898d84625e43acb9787
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-19 22:46:08 +0100

    Fix build.zig

--- build.zig
+++ build.zig
@@ -1,4 +1,21 @@
 const std = @import("std");
 
-pub fn build(_: *std.Build) void{
+pub fn build(b: *std.Build) void{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const libfizzbuzz = b.addLibrary(.{
+        .name = "mod_pkcs11",
+        .linkage = .dynamic,
+        .version = .{ .major = 1, .minor = 2, .patch = 3 },
+        .root_module = mod,
+    });
+
+    b.installArtifact(libfizzbuzz);
 }
