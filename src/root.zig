49c0145f56fe14777b7efdc2418834b8fe415d4b
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-20 00:46:42 +0100

    Add output

--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,15 @@
 const pkcs11: type = @import("pkcs11.zig");
+const std: type = @import("std");
 
-export fn C_Initialize(pInitArgs: pkcs11.CK_VOID_PTR) pkcs11.CK_RV {
-    pInitArgs.*;
-    return 0;
-}
+// export fn C_Initialize(pInitArgs: pkcs11.CK_VOID_PTR) pkcs11.CK_RV {
+//     pInitArgs.*;
+//     return 0;
+// }
+
 
 export fn C_GetFunctionList(ppFunctionList: pkcs11.CK_FUNCTION_LIST_PTR_PTR) pkcs11.CK_RV {
-    ppFunctionList.*.*.C_Initialize = C_Initialize;
-    return 0;
+    std.debug.print("[CALLED]: C_GetFunctionList\n",.{});
+    std.debug.print(" - Version {}\n",.{ppFunctionList});
+    // ppFunctionList.*.*.C_Initialize = C_Initialize;
+    return 123;
 }
