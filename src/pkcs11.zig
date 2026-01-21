cca3446deb2d387b5b4d19404f24fd6b27a4ec57
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-21 15:23:49 +0100

    Make function list undefined first.

--- src/pkcs11.zig
+++ src/pkcs11.zig
@@ -45,7 +45,7 @@ const TRUE: CK_BBOOL = CK_TRUE;
 
 // Section 3.1
 
-const CK_VERSION: type = struct {
+pub const CK_VERSION: type = extern struct {
     major: CK_BYTE,
     minor: CK_BYTE,
 };
@@ -62,19 +62,148 @@ pub const CK_RV: type = CK_ULONG;
 
 //const CK_NOTIFY: type;//TODO
 
-const CK_C_Initialize: type = *const fn(pInitArgs: CK_VOID_PTR) callconv(.c) CK_RV;
-const CK_C_Finalize: type = *fn(pReserved: CK_VOID_PTR) CK_RV;
-//const CK_C_GetInfo: type = *fn(pInfo: CK_INFO_PTR) CK_RV;
-const CK_C_GetFunctionList: type = *fn(ppFunctionList: CK_FUNCTION_LIST_PTR_PTR) CK_RV;
-//const CK_C_GetSlotList: type = *fn(tokenPresent: CK_BBOOL,pSlotList: CK_SLOT_ID_PTR,pulCount: CK_ULONG_PTR) CK_RV;
+const CK_C_Initialize: type = *const fn(pInitArgs: CK_VOID_PTR) CK_RV;
+const CK_C_Finalize: type = *const fn(pReserved: CK_VOID_PTR) CK_RV;
+const CK_C_GetInfo: type = *const fn() CK_RV; //TODO
+//const CK_C_GetFunctionList: type = *const fn(ppFunctionList: CK_FUNCTION_LIST_PTR_PTR) CK_RV;
+const CK_C_GetFunctionList: type = *const fn(ppFunctionList: *?*CK_FUNCTION_LIST) CK_RV;
+const CK_C_GetSlotList: type = *const fn() CK_RV; //TODO
+const CK_C_GetSlotInfo: type = *const fn() CK_RV; //TODO
+const CK_C_GetTokenInfo: type = *const fn() CK_RV; //TODO
+const CK_C_GetMechanismList: type = *const fn() CK_RV; //TODO
+const CK_C_GetMechanismInfo: type = *const fn() CK_RV; //TODO
+const CK_C_InitToken: type = *const fn() CK_RV; //TODO
+const CK_C_InitPIN: type = *const fn() CK_RV; //TODO
+const CK_C_SetPIN: type = *const fn() CK_RV; //TODO
+const CK_C_OpenSession: type = *const fn() CK_RV; //TODO
+const CK_C_CloseSession: type = *const fn() CK_RV; //TODO
+const CK_C_CloseAllSessions: type = *const fn() CK_RV; //TODO
+const CK_C_GetSessionInfo: type = *const fn() CK_RV; //TODO
+const CK_C_GetOperationState: type = *const fn() CK_RV; //TODO
+const CK_C_SetOperationState: type = *const fn() CK_RV; //TODO
+const CK_C_Login: type = *const fn() CK_RV; //TODO
+const CK_C_Logout: type = *const fn() CK_RV; //TODO
+const CK_C_CreateObject: type = *const fn() CK_RV; //TODO
+const CK_C_CopyObject: type = *const fn() CK_RV; //TODO
+const CK_C_DestroyObject: type = *const fn() CK_RV; //TODO
+const CK_C_GetObjectSize: type = *const fn() CK_RV; //TODO
+const CK_C_GetAttributeValue: type = *const fn() CK_RV; //TODO
+const CK_C_SetAttributeValue: type = *const fn() CK_RV; //TODO
+const CK_C_FindObjectsInit: type = *const fn() CK_RV; //TODO
+const CK_C_FindObjects: type = *const fn() CK_RV; //TODO
+const CK_C_FindObjectsFinal: type = *const fn() CK_RV; //TODO
+const CK_C_EncryptInit: type = *const fn() CK_RV; //TODO
+const CK_C_Encrypt: type = *const fn() CK_RV; //TODO
+const CK_C_EncryptUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_EncryptFinal: type = *const fn() CK_RV; //TODO
+const CK_C_DecryptInit: type = *const fn() CK_RV; //TODO
+const CK_C_Decrypt: type = *const fn() CK_RV; //TODO
+const CK_C_DecryptUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_DecryptFinal: type = *const fn() CK_RV; //TODO
+const CK_C_DigestInit: type = *const fn() CK_RV; //TODO
+const CK_C_Digest: type = *const fn() CK_RV; //TODO
+const CK_C_DigestUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_DigestKey: type = *const fn() CK_RV; //TODO
+const CK_C_DigestFinal: type = *const fn() CK_RV; //TODO
+const CK_C_SignInit: type = *const fn() CK_RV; //TODO
+const CK_C_Sign: type = *const fn() CK_RV; //TODO
+const CK_C_SignUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_SignFinal: type = *const fn() CK_RV; //TODO
+const CK_C_SignRecoverInit: type = *const fn() CK_RV; //TODO
+const CK_C_SignRecover: type = *const fn() CK_RV; //TODO
+const CK_C_VerifyInit: type = *const fn() CK_RV; //TODO
+const CK_C_Verify: type = *const fn() CK_RV; //TODO
+const CK_C_VerifyUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_VerifyFinal: type = *const fn() CK_RV; //TODO
+const CK_C_VerifyRecoverInit: type = *const fn() CK_RV; //TODO
+const CK_C_VerifyRecover: type = *const fn() CK_RV; //TODO
+const CK_C_DigestEncryptUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_DecryptDigestUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_SignEncryptUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_DecryptVerifyUpdate: type = *const fn() CK_RV; //TODO
+const CK_C_GenerateKey: type = *const fn() CK_RV; //TODO
+const CK_C_GenerateKeyPair: type = *const fn() CK_RV; //TODO
+const CK_C_WrapKey: type = *const fn() CK_RV; //TODO
+const CK_C_UnwrapKey: type = *const fn() CK_RV; //TODO
+const CK_C_DeriveKey: type = *const fn() CK_RV; //TODO
+const CK_C_SeedRandom: type = *const fn() CK_RV; //TODO
+const CK_C_GenerateRandom: type = *const fn() CK_RV; //TODO
+const CK_C_GetFunctionStatus: type = *const fn() CK_RV; //TODO
+const CK_C_CancelFunction: type = *const fn() CK_RV; //TODO
+const CK_C_WaitForSlotEvent: type = *const fn() CK_RV; //TODO
 
-const CK_FUNCTION_LIST: type = struct {
+pub const CK_FUNCTION_LIST: type = extern struct {
     version: CK_VERSION,
-    C_Initialize: CK_C_Initialize,
-    C_Finalize: CK_C_Finalize,
-    //C_GetInfo: CK_C_GetInfo,
+    C_Initialize: *const fn (*void) c_ulong, //*const fn (*void) c_ulong,//CK_C_Initialize, *const fn (*void) c_ulong *const fn(*void) c_ulong
+    //C_Finalize: u64,//CK_C_Finalize,
+    //C_GetInfo: u64,//CK_C_GetInfo, *const fn (*void) c_ulong
+    //C_GetFunctionList: u64,//CK_C_GetFunctionList,
+    // C_GetSlotList: CK_C_GetSlotList,
+    // C_GetSlotInfo: CK_C_GetSlotInfo,
+    // C_GetTokenInfo: CK_C_GetTokenInfo,
+    // C_GetMechanismList: CK_C_GetMechanismList,
+    // C_GetMechanismInfo: CK_C_GetMechanismInfo,
+    // C_InitToken: CK_C_InitToken,
+    // C_InitPIN: CK_C_InitPIN,
+    // C_SetPIN: CK_C_SetPIN,
+    // C_OpenSession: CK_C_OpenSession,
+    // C_CloseSession: CK_C_CloseSession,
+    // C_CloseAllSessions: CK_C_CloseAllSessions,
+    // C_GetSessionInfo: CK_C_GetSessionInfo,
+    // C_GetOperationState: CK_C_GetOperationState,
+    // C_SetOperationState: CK_C_SetOperationState,
+    // C_Login: CK_C_Login,
+    // C_Logout: CK_C_Logout,
+    // C_CreateObject: CK_C_CreateObject,
+    // C_CopyObject: CK_C_CopyObject,
+    // C_DestroyObject: CK_C_DestroyObject,
+    // C_GetObjectSize: CK_C_GetObjectSize,
+    // C_GetAttributeValue: CK_C_GetAttributeValue,
+    // C_SetAttributeValue: CK_C_SetAttributeValue,
+    // C_FindObjectsInit: CK_C_FindObjectsInit,
+    // C_FindObjects: CK_C_FindObjects,
+    // C_FindObjectsFinal: CK_C_FindObjectsFinal,
+    // C_EncryptInit: CK_C_EncryptInit,
+    // C_Encrypt: CK_C_Encrypt,
+    // C_EncryptUpdate: CK_C_EncryptUpdate,
+    // C_EncryptFinal: CK_C_EncryptFinal,
+    // C_DecryptInit: CK_C_DecryptInit,
+    // C_Decrypt: CK_C_Decrypt,
+    // C_DecryptUpdate: CK_C_DecryptUpdate,
+    // C_DecryptFinal: CK_C_DecryptFinal,
+    // C_DigestInit: CK_C_DigestInit,
+    // C_Digest: CK_C_Digest,
+    // C_DigestUpdate: CK_C_DigestUpdate,
+    // C_DigestKey: CK_C_DigestKey,
+    // C_DigestFinal: CK_C_DigestFinal,
+    // C_SignInit: CK_C_SignInit,
+    // C_Sign: CK_C_Sign,
+    // C_SignUpdate: CK_C_SignUpdate,
+    // C_SignFinal: CK_C_SignFinal,
+    // C_SignRecoverInit: CK_C_SignRecoverInit,
+    // C_SignRecover: CK_C_SignRecover,
+    // C_VerifyInit: CK_C_VerifyInit,
+    // C_Verify: CK_C_Verify,
+    // C_VerifyUpdate: CK_C_VerifyUpdate,
+    // C_VerifyFinal: CK_C_VerifyFinal,
+    // C_VerifyRecoverInit: CK_C_VerifyRecoverInit,
+    // C_VerifyRecover: CK_C_VerifyRecover,
+    // C_DigestEncryptUpdate: CK_C_DigestEncryptUpdate,
+    // C_DecryptDigestUpdate: CK_C_DecryptDigestUpdate,
+    // C_SignEncryptUpdate: CK_C_SignEncryptUpdate,
+    // C_DecryptVerifyUpdate: CK_C_DecryptVerifyUpdate,
+    // C_GenerateKey: CK_C_GenerateKey,
+    // C_GenerateKeyPair: CK_C_GenerateKeyPair,
+    // C_WrapKey: CK_C_WrapKey,
+    // C_UnwrapKey: CK_C_UnwrapKey,
+    // C_DeriveKey: CK_C_DeriveKey,
+    // C_SeedRandom: CK_C_SeedRandom,
+    // C_GenerateRandom: CK_C_GenerateRandom,
+    // C_GetFunctionStatus: CK_C_GetFunctionStatus,
+    // C_CancelFunction: CK_C_CancelFunction,
+    // C_WaitForSlotEvent: CK_C_WaitForSlotEvent,
 };
 
-const CK_FUNCTION_LIST_PTR: type = *CK_FUNCTION_LIST;
+pub const CK_FUNCTION_LIST_PTR: type = *CK_FUNCTION_LIST;
 
-pub const CK_FUNCTION_LIST_PTR_PTR: type = *CK_FUNCTION_LIST_PTR;
+pub const CK_FUNCTION_LIST_PTR_PTR: type = ?(*CK_FUNCTION_LIST_PTR);
--- src/root.zig
+++ src/root.zig
@@ -1,15 +1,133 @@
 const pkcs11: type = @import("pkcs11.zig");
 const std: type = @import("std");
 
-// export fn C_Initialize(pInitArgs: pkcs11.CK_VOID_PTR) pkcs11.CK_RV {
-//     pInitArgs.*;
-//     return 0;
-// }
-
-
-export fn C_GetFunctionList(ppFunctionList: pkcs11.CK_FUNCTION_LIST_PTR_PTR) pkcs11.CK_RV {
-    std.debug.print("[CALLED]: C_GetFunctionList\n",.{});
-    std.debug.print(" - Version {}\n",.{ppFunctionList});
-    // ppFunctionList.*.*.C_Initialize = C_Initialize;
+export fn C_Initialize(_: pkcs11.CK_VOID_PTR) pkcs11.CK_RV {
     return 123;
+    // std.debug.print("[CALLED]: C_Initialize()\n",.{});
+    // std.debug.print("[CALLED]: C_Initialize({})\n",.{pInitArgs});
+    // //pInitArgs.*;
+    // return 0;
 }
+
+export fn C_Finalize(pReserved: pkcs11.CK_VOID_PTR) pkcs11.CK_RV {
+    std.debug.print("[CALLED]: C_Finalize()\n",.{});
+    std.debug.print("[CALLED]: C_Finalize({})\n",.{pReserved});
+    //TODO
+    pReserved.*;
+    return 0;
+}
+
+export fn todoFunc() pkcs11.CK_RV {//notSupported
+    std.debug.print("[CALLED]\n",.{});
+    return 0x54;
+}
+
+const version: pkcs11.CK_VERSION = pkcs11.CK_VERSION{
+    .major = 0x02,
+    .minor = 0x28,
+};
+
+var functionList: pkcs11.CK_FUNCTION_LIST = undefined;
+
+
+
+//pkcs11.CK_FUNCTION_LIST_PTR_PTR
+////ppFunctionList: *pkcs11.CK_FUNCTION_LIST
+export fn C_GetFunctionList(ppFunctionList: *?*pkcs11.CK_FUNCTION_LIST) pkcs11.CK_RV {
+    std.debug.print("[CALLED]: C_GetFunctionList(ppFunctionList={})\n",.{ppFunctionList});
+    std.debug.print(" - ppFunctionList = {} \n",.{@intFromPtr(ppFunctionList)});
+    std.debug.print(" - *ppFunctionList = {} \n",.{@intFromPtr(ppFunctionList.*)});
+
+    //if(ppFunctionList==null){
+    //    std.debug.print(" - ppFunctionList IS NULL\n",.{});
+    //    return 123;
+    //}
+
+    // ppFunctionList.* = null;
+
+    //functionList.version = C_Initialize;
+    functionList.C_Initialize = C_Initialize;
+
+    ppFunctionList.* = &functionList;
+
+    // std.debug.print(" - ppFunctionList = {} \n",.{@intFromPtr(ppFunctionList)});
+    // std.debug.print(" - *ppFunctionList = {} \n",.{@intFromPtr(ppFunctionList.*)});
+    // std.debug.print(" - C_Initialize = {} \n",.{ppFunctionList.*.?.C_Initialize});
+
+    return 0;
+}
+
+
+//
+// pkcs11.CK_FUNCTION_LIST{
+// .version = version,
+// .C_Initialize = C_Initialize,
+// //.C_Initialize = &C_Initialize,
+// //.C_Finalize = 123,
+// //.C_GetInfo = &C_Initialize,
+// //.C_GetFunctionList = 123,
+// // .C_GetSlotList = todoFunc,
+// // .C_GetSlotInfo = todoFunc,
+// // .C_GetTokenInfo = todoFunc,
+// // .C_GetMechanismList = todoFunc,
+// // .C_GetMechanismInfo = todoFunc,
+// // .C_InitToken = todoFunc,
+// // .C_InitPIN = todoFunc,
+// // .C_SetPIN = todoFunc,
+// // .C_OpenSession = todoFunc,
+// // .C_CloseSession = todoFunc,
+// // .C_CloseAllSessions = todoFunc,
+// // .C_GetSessionInfo = todoFunc,
+// // .C_GetOperationState = todoFunc,
+// // .C_SetOperationState = todoFunc,
+// // .C_Login = todoFunc,
+// // .C_Logout = todoFunc,
+// // .C_CreateObject = todoFunc,
+// // .C_CopyObject = todoFunc,
+// // .C_DestroyObject = todoFunc,
+// // .C_GetObjectSize = todoFunc,
+// // .C_GetAttributeValue = todoFunc,
+// // .C_SetAttributeValue = todoFunc,
+// // .C_FindObjectsInit = todoFunc,
+// // .C_FindObjects = todoFunc,
+// // .C_FindObjectsFinal = todoFunc,
+// // .C_EncryptInit = todoFunc,
+// // .C_Encrypt = todoFunc,
+// // .C_EncryptUpdate = todoFunc,
+// // .C_EncryptFinal = todoFunc,
+// // .C_DecryptInit = todoFunc,
+// // .C_Decrypt = todoFunc,
+// // .C_DecryptUpdate = todoFunc,
+// // .C_DecryptFinal = todoFunc,
+// // .C_DigestInit = todoFunc,
+// // .C_Digest = todoFunc,
+// // .C_DigestUpdate = todoFunc,
+// // .C_DigestKey = todoFunc,
+// // .C_DigestFinal = todoFunc,
+// // .C_SignInit = todoFunc,
+// // .C_Sign = todoFunc,
+// // .C_SignUpdate = todoFunc,
+// // .C_SignFinal = todoFunc,
+// // .C_SignRecoverInit = todoFunc,
+// // .C_SignRecover = todoFunc,
+// // .C_VerifyInit = todoFunc,
+// // .C_Verify = todoFunc,
+// // .C_VerifyUpdate = todoFunc,
+// // .C_VerifyFinal = todoFunc,
+// // .C_VerifyRecoverInit = todoFunc,
+// // .C_VerifyRecover = todoFunc,
+// // .C_DigestEncryptUpdate = todoFunc,
+// // .C_DecryptDigestUpdate = todoFunc,
+// // .C_SignEncryptUpdate = todoFunc,
+// // .C_DecryptVerifyUpdate = todoFunc,
+// // .C_GenerateKey = todoFunc,
+// // .C_GenerateKeyPair = todoFunc,
+// // .C_WrapKey = todoFunc,
+// // .C_UnwrapKey = todoFunc,
+// // .C_DeriveKey = todoFunc,
+// // .C_SeedRandom = todoFunc,
+// // .C_GenerateRandom = todoFunc,
+// // .C_GetFunctionStatus = todoFunc,
+// // .C_CancelFunction = todoFunc,
+// // .C_WaitForSlotEvent = todoFunc,
+// }
